15441394febadc7d81a2d39722562d1f7aa277d5
Benjamin Aalto <benjamin.aalto@hotmail.com>
2025-04-25 21:39:27 +0300

    Making boat changes

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,38 +4,42 @@ public class playerController : MonoBehaviour
 {
     private float kääntyvyys = 0;
     private float kiihtyvyys = 0;
-    public float maxKiihtyvyys = 1;
+    public float maxKiihtyvyys = 10;
     public float maxKääntyvyys = 1;
 
     private bool isInWater = false;
     public Rigidbody shipRB;
-
-    // Update is called once per frame
     void FixedUpdate()
     {
+        // -------- KIIHTYVYYS -------------
         if(Input.GetKey("w") && kiihtyvyys < maxKiihtyvyys) {
-            kiihtyvyys += maxKiihtyvyys / 10;
+            kiihtyvyys += maxKiihtyvyys / 20;
         }
         if(Input.GetKey("s") && kiihtyvyys > 0) {
-            kiihtyvyys -= maxKiihtyvyys / 10;
+            kiihtyvyys -= maxKiihtyvyys / 20;
             if(kiihtyvyys < 0) kiihtyvyys = 0;
         }
-        print(kiihtyvyys);
-        if(kääntyvyys > maxKääntyvyys && kääntyvyys < -maxKääntyvyys) {
-            if(Input.GetKey(KeyCode.A)) {
-                kääntyvyys += maxKääntyvyys / 10;
-                print("Kääntyy!");
-            }
-            if(Input.GetKey(KeyCode.D)) {
-                kääntyvyys -= maxKääntyvyys / 10;
-                print("Kääntyy!");
-            }
-        }
         shipRB.AddForce(transform.forward * kiihtyvyys, ForceMode.Force);
+        // -------- KIIHTYVYYS -------------
+        // -------- KÄÄNTYVYYS -------------
+        if(Input.GetKey("a") && kääntyvyys > maxKiihtyvyys) {
+            kääntyvyys -= maxKääntyvyys / 10;
+        }
+        if(Input.GetKey("d") && kääntyvyys < -maxKiihtyvyys) {
+            kääntyvyys += maxKääntyvyys / 10;
+        }
+        Vector3 laivanEulerKääntyvyys = new Vector3(0, kääntyvyys, 0);
+        Quaternion deltaKääntyvyys = Quaternion.Euler(laivanEulerKääntyvyys * Time.fixedDeltaTime);
+        shipRB.MoveRotation(shipRB.rotation * deltaKääntyvyys);
+
+        // -------- KÄÄNTYVYYS -------------
+
+        // -------- Kelluvuus --------------
         if(isInWater) 
-            shipRB.AddForce(transform.up * (((0 - transform.position.y))*((0 - transform.position.y))), ForceMode.Force);
+            shipRB.AddForce(transform.up * 4 * (((0 - transform.position.y))*((0 - transform.position.y))), ForceMode.Force);
         if(transform.position.y > 1)
             shipRB.AddForce(transform.up * -1, ForceMode.VelocityChange);
+        // -------- Kelluvuus ---------------
     }
     void OnTriggerEnter(Collider collider) {
         if(collider.tag == "Water") {
