4d7755542c15c70f10962b9cd0613ad184c49627
William Matikainen <William.Matikainen@edu.mayk.fi>
2025-04-25 11:14:51 +0300

    Water changes & update

--- Assets/Scripts/BoatController.cs
+++ Assets/Scripts/BoatController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class waterScript : MonoBehaviour
+public class BoatController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
